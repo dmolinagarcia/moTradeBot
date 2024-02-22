7c7e80ba899fd6054f59589e531e27db4a141cca
Daniel Molina <dmolina@gmail.com>
2024-02-22 12:10:48 +0000

    install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,15 +1,10 @@
 sudo apt --assume-yes update
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update && 
-    sudo apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y --force-yes &&
-    sudo apt-get -o Dpkg::Options::="--force-confold" dist-upgrade -q -y --force-yes
+sudo NEEDRESTART_MODE=a apt-get dist-upgrade --yes
 sudo apt-get --assume-yes install software-properties-common
 sudo add-apt-repository universe
 sudo apt-get --assume-yes install git python3 vim bsdmainutils sqlite3 python3-pip jq nodejs python cron
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update && 
-    sudo apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y --force-yes &&
-    sudo apt-get -o Dpkg::Options::="--force-confold" dist-upgrade -q -y --force-yes
+sudo NEEDRESTART_MODE=a apt-get dist-upgrade --yes
 umask 027
 sudo ln -s -f /usr/bin/python3.8 /usr/bin/python
 sudo pip3 install --target /lib/python3.8 --upgrade testresources Django json2html flask 
