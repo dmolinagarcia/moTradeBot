af034ac980906e70baac3c4537c287129dda5f00
Daniel Molina <dmolina@gmail.com>
2024-02-23 13:33:11 +0000

    install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -13,8 +13,8 @@ sudo NEEDRESTART_MODE=a apt-get --assume-yes install software-properties-common
 sudo add-apt-repository --yes universe
 sudo NEEDRESTART_MODE=a apt-get --assume-yes install git python3 vim bsdmainutils sqlite3 python3-pip jq nodejs python2 cron 
 umask 027
-sudo ln -s -f /usr/bin/python3.8 /usr/bin/python
-sudo pip3 install --target /lib/python3 --upgrade testresources Django json2html flask 
+sudo ln -s -f /usr/bin/python3 /usr/bin/python
+sudo -u moTrade pip3 install --target /lib/python3 --upgrade testresources Django json2html flask
 ## sudo pip3 install git+git://github.com/Lu-Yi-Hsun/iqoptionapi.git
 ## IQOption no longer supported
 echo "Europe/Madrid" | sudo tee /etc/timezone
@@ -32,7 +32,7 @@ sudo NEEDRESTART_MODE=a apt-get dist-upgrade --yes
 ##
 
 ## Generate BINGXCFG.py
-whiptail --msgbox --title "Please enter your BINGX credentials" "This information won't ever be shared with anyone" 8 80
+whiptail --msgbox --title "Please enter your BINGX credentials" "This information won't ever be shared with anyone and will be kept secure withing your own server!" 8 80
 vAPIKEY=$(whiptail --inputbox "Enter your BINGx APIKEY" 8  80 3>&1 1>&2 2>&3)
 vSECRETKEY=$(whiptail --inputbox "Enter your BINGx SECRETKEY" 8  80 3>&1 1>&2 2>&3)
 vACCOUNT=$(whiptail --radiolist --title "Use REAL or TEST account?" "Please select your preferred option" 8 80 2 "REAL" "Use your REAL account" OFF  "TEST" "Use your TEST account" ON  3>&1 1>&2 2>&3)
