068ac728eae05173595b7cdc46d400b4bedd66df
Daniel Molina <dmolina@gmail.com>
2024-03-05 09:29:36 +0100

    Update update.sh

--- setup/update.sh
+++ setup/update.sh
@@ -2,11 +2,12 @@
 
 cd /home/ubuntu
 date
+date  >> /home/ubuntu/update.debug.log 2>&1
 rm -rf moTradeBot
-git clone https://github.com/dmolinagarcia/moTradeBot.git >> /dev/null 2>&1
+git clone https://github.com/dmolinagarcia/moTradeBot.git  >> /home/ubuntu/update.debug.log 2>&1
 cd moTradeBot
 vLATESTTAG=$(git tag --sort=creatordate | tail -1)
-vCURRENTTAG=$(sudo -u moTrade sh -c "cd /home/moTrade; git tag | tail -1")
+vCURRENTTAG=$(sudo -u moTrade sh -c "cd /home/moTrade; git tag --sort=creatordate | tail -1")
 
 cd /home/ubuntu/moTradeBot
 git checkout $vLATESTTAG >> /dev/null 2>&1
@@ -25,10 +26,10 @@ else
         rm -rf /home/ubuntu/moTradeBot
         echo Updated
         sudo systemctl stop apache2
-        sudo -u moTrade sh -c "cd /home/moTrade; python ./manage.py makemigrations" >> /dev/null 2>&1
-        sudo -u moTrade sh -c "cd /home/moTrade; python ./manage.py migrate"  >> /dev/null 2>&1
+        sudo -u moTrade sh -c "cd /home/moTrade; python ./manage.py makemigrations --merge" >> /home/ubuntu/update.debug.log 2>&1
+        sudo -u moTrade sh -c "cd /home/moTrade; python ./manage.py migrate"   >> /home/ubuntu/update.debug.log 2>&1
         sudo systemctl start apache2
         sudo systemctl restart BINGX.service
-        vCURRENTTAG=$(sudo -u moTrade sh -c "cd /home/moTrade; git tag | tail -1")
+        vCURRENTTAG=$(sudo -u moTrade sh -c "cd /home/moTrade; git tag --sort=creatordate| tail -1")
         echo Current: $vCURRENTTAG Latest: $vLATESTTAG
 fi
